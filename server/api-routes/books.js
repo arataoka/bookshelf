b183b12a2b8959dd02d9bae5fde0833dbf232c5f
arataoka <arata.okayasu@gmail.com>
2023-02-26 19:40:58 +0900

    feat: create rest api

--- server/api-routes/books.js
+++ server/api-routes/books.js
@@ -5,8 +5,41 @@ const router = express.Router();
 
 // /api/books
 router.get("/", async (req,res)=>{
-    const books = await Book.find();
+    const books = await Book.find().sort({updatedAt:-1}); // 降順全て取得
     res.json(books);
+})
+
+router.get("/:id", async (req,res)=>{
+    const _id = req.params.id; // queryから取得
+    // const book = await Book.findOne({_id});
+    const book = await Book.findById(_id);
+    res.json(book);
+
+})
+
+router.delete("/:id", async (req,res)=>{
+    const _id = req.params.id; // queryから取得
+    const book = await Book.findByIdAndDelete(_id);
+    res.json({"msg":`${book.title} is deleted`});
+})
+
+router.patch("/:id", async (req,res)=>{
+    const {title,description,comment,rating} = req.body;
+    const _id = req.params.id; // queryから取得
+    const book = await Book.findById(_id);
+    book.title = title;
+    book.description = description;
+    book.comment = comment;
+    book.rating = rating;
+    await book.save();
+    res.json(book);
+})
+
+
+router.post("/", async (req,res)=>{
+    const book = new Book(req.body);
+    const newBook = await book.save();
+    res.json(newBook)
 })
 
 export default  router;
